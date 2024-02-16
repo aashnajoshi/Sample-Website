a3094370d680d6310f6250ac9d0c60c0f3d70b61
aashnajoshi <aashnahetal@gmail.com>
2024-02-16 14:44:36 +0530

    .

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
+const slides = document.querySelectorAll(".slides");
 let slideIndex = 0;
+
 showSlides();
 
 function showSlides() {
-    let slides = document.getElementsByClassName("slides");
-
-    // Hide all slides
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    // Move to the next slide
+    slides.forEach(slide => slide.style.display = "none");
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-
-    // Call showSlides() again after 3 seconds
-    setTimeout(showSlides, 3000); // Change slide every 3 seconds
+    setTimeout(showSlides, 3000);
 }
