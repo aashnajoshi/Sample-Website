67097483d005958d09a8e98464beebb6370e30a3
aashnajoshi <aashnahetal@gmail.com>
2024-02-20 00:49:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,18 +4,15 @@ const buttonTexts = ["Software Development", "Web Development", "UI/UX Design", 
 let buttonTextIndex = 0;
 
 showSlides();
-changeButtonText(); // Start changing button text immediately
+changeButtonText();
 
 function showSlides() {
     slides.forEach(slide => slide.style.display = "none");
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-
-    // Update button text with corresponding index
     const welcomeTextPlaceholder = slides[slideIndex - 1].querySelector(".welcome-text");
-    welcomeTextPlaceholder.innerHTML = generateWelcomeText("Lorem ipsum <u>dolor</u> sit amet.", buttonTexts[buttonTextIndex]);
-
+    welcomeTextPlaceholder.innerHTML = generateWelcomeText("Work to put <u>spotlight</u> on projects like", buttonTexts[buttonTextIndex]);
     setTimeout(showSlides, 3000);
 }
 
@@ -24,13 +21,12 @@ function generateWelcomeText(subtitle, button) {
         <p>We at</p>
         <h1>BRIX</h1>
         <p>${subtitle}</p><br>
-        <button type="button" id="dynamic-button">${button}</button>
-    `;
+        <button type="button" id="dynamic-button">${button}</button>`;
 }
 
 function changeButtonText() {
     const dynamicButton = document.getElementById("dynamic-button");
     dynamicButton.innerHTML = buttonTexts[buttonTextIndex];
-    buttonTextIndex = (buttonTextIndex + 1) % buttonTexts.length; // Move to the next index cyclically
-    setTimeout(changeButtonText, 3000); // Call the function again after 3 seconds
+    buttonTextIndex = (buttonTextIndex + 1) % buttonTexts.length;
+    setTimeout(changeButtonText, 3000);
 }
